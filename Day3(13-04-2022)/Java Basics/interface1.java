8a446ddd814007556d08d05dfd1ca75c6b39dc22
Chilmakuri Narasimha sai <74558094+Narasimha13009@users.noreply.github.com>
2022-04-14 10:32:22 +0530

    Add files via upload

---
+++ Day3(13-04-2022)/Java Basics/interface1.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public interface interface1 {
+    final int a = 10;
+    void show();
+}
+
+class Main implements interface1{
+	public void show() {
+		System.out.println("implemented interface");
+	}
+	public static void main(String[] args) {
+		Main m1 = new Main();
+		m1.show();
+		System.out.println(a);
+	}
+}
